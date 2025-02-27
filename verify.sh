aba6997587d17e43ce7298d0818d231a12d7a95a
John Comeau <jc@unternet.net>
2025-02-27 15:14:22 -0700

    more of a proper script now, takes args as alternative to envvars

--- verify.sh
+++ verify.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
-if [ -e "$KEYFILE" ] && [ -e $CERTFILE ]; then
+if [ -e "$1" ]; then
+	if [ -z "$KEYFILE" ]; then
+		KEYFILE=$1
+	elif [ $KEYFILE != $1 ]; then
+		echo keyfile from env and commandline differ >&2
+		exit 1
+	fi
+fi
+if [ -e "$2" ]; then
+	if [ -z "$CERTFILE" ]; then
+		CERTFILE=$2
+	elif [ $CERTFILE != $2 ]; then
+		echo certfile from env and commandline differ >&2
+		exit 1
+	fi
+fi
+if [ -e "$KEYFILE" ] && [ -e "$CERTFILE" ]; then
 	echo found $KEYFILE and $CERTFILE
 else
-	echo did not find one or more necessary files >&2
+	echo Usage $0 keyfile certfile [satpassword] >&2
 	exit 1
 fi
 if [ -z "$SATPASS" ] || [ $SATPASS = "pUtPa55w0rDh3rE" ] \
 	|| [ $SATPASS = "MySecretPassword" ]; then
 	echo invalid password, please read the Makefile >&2
-	exit 1
+	read -e -p "input correct password: " SATPASS
 fi
 openssl pkcs8 -inform DER -in $KEYFILE -out $KEYFILE.pem -passin pass:$SATPASS
 openssl x509 -inform DER -outform PEM -in $CERTFILE -pubkey -out $CERTFILE.pem
-echo 4xBbCfSj | openssl pkcs12 -export -inkey $KEYFILE.pem -in $CERTFILE.pem -out $KEYFILE.pfx -passout pass:$SATPASS
+openssl pkcs12 -export -inkey $KEYFILE.pem -in $CERTFILE.pem \
+ -out $KEYFILE.pfx -passout pass:$SATPASS
 openssl x509 -in $CERTFILE.pem -noout -serial
 openssl x509 -in $CERTFILE.pem -noout -startdate
 openssl x509 -in $CERTFILE.pem -noout -enddate
 openssl x509 -in $CERTFILE.pem -noout -subject
 # generate pkcs12 combined cert and key for gpgsm
 # https://stackoverflow.com/a/62613267/493161
-openssl pkcs12 -export -in $CERTFILE.pem -inkey $KEYFILE.pem -out $KEYFILE.p12
+#openssl pkcs12 -export -in $CERTFILE.pem -inkey $KEYFILE.pem -out $KEYFILE.p12
 modcert=$(openssl x509 -noout -modulus -in $CERTFILE.pem)
 modkey=$(openssl rsa -noout -modulus -in $KEYFILE.pem)
 if [ -n "$modcert" ]; then
